5321ee3667143cccc2485ee0ca4db94a8997eda8
Mio Carletta <mio.carletta@gmail.com>
2026-04-18 21:56:43 +0700

    Create characters.js

---
+++ data/characters.js
@@ -0,0 +1,8 @@
+const characters = {
+  Mio: { role: "Analyzer" },
+  Pena: { role: "Strategist" },
+  Vania: { role: "Illusion" },
+  Mizu: { role: "Balance" },
+  Pulu: { role: "Abstract" },
+  Al: { role: "Trickster" }
+};
