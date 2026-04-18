d73a9a48024b87697dd3d68111e06c6497816ca7
Mio Carletta <mio.carletta@gmail.com>
2026-04-18 21:58:26 +0700

    Create puzzle.js

---
+++ data/puzzle.js
@@ -0,0 +1,18 @@
+const puzzles = {
+
+  Mio: {
+    q: "2, 4, 8, 16, ?",
+    a: "32"
+  },
+
+  Al: {
+    q: "Jangan jawab benar 😏",
+    a: "salah"
+  },
+
+  default: {
+    q: "Kamu merasa dunia ini aneh...",
+    a: "apa saja"
+  }
+
+};
